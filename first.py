8a308d8254324bb5990b995de92fdb54cb51e8c0
abhichandra007 <127487079+abhichandra007@users.noreply.github.com>
2023-03-10 15:26:12 +0530

    Create first.py

---
+++ first.py
@@ -0,0 +1,51 @@
+import psycopg2
+class Comp:
+    def __init__(self,table) :
+        self.connection = psycopg2.connect(user='postgres', password='1234', host='localhost', port=5432, database='northwind')
+        self.table = table
+
+
+    def create_table(self, values={'name': 'varchar(50)', 'adress': 'varchar(50)', 'phone': 'int'}):
+
+        cursor=self.connection.cursor()
+        res = []
+        for i, j in values.items():
+            res.append(str(i)+' '+str(j))
+
+        query = f"""create table {self.table} ({',  '.join(res)})"""
+        cursor.execute(query)
+        self.connection.commit
+        print("table created successfully")  
+
+
+    def insert_table(self,table):
+        cursor = self.connection.cursor()
+        query = f"""insert into {table} values ('oneplus', 'hyd'), ('mi', 'rr'), ('sy', 'ben'), ('da', 'jksf'), ('uhfsuah', 'jhbfsd'), ('js', 'jusafdn')"""
+        cursor.execute(query)
+        self.connection.commit()
+        return "Table inserted successfully"
+    
+
+
+
+
+    def update_table(self, values):
+        cursor = self.connection.cursor()
+        res = []
+        for i, j in values.items():
+            res.append(str(i) + " = '" + str(j) + "'")
+        set = ", ".join(res)
+        query = f"update {self.table} set {set}"
+        cursor.execute(query)
+        self.connection.commit()
+        print( "Table updated successfully")
+
+
+
+
+
+    def delete_table(self,values):
+        cursor = self.connection.cursor()
+        res= []
+        for i,j in values.items():
+            res.drop(str(i))
